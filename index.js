d652a577ee6f16c47817a2d6f9ae1b76a03b0a38
FizziButt <153606560+FizziButt@users.noreply.github.com>
2025-05-02 19:30:36 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const msgpack = require('msgpack-lite');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/msgpack', (req, res) => {
+    const text = req.body.text
+  const payload = { text: text };
+  const buffer = msgpack.encode(payload);
+
+  res.set('Content-Type', 'application/msgpack');
+  res.send(buffer);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
